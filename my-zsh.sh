0889a1d6179ac094a902fffd3c8d1933c9118115
郭世雄 <guoshixiong@zmeng123.com>
2020-06-17 11:38:07 +0800

    init

---
+++ my-zsh.sh
@@ -0,0 +1,18 @@
+#
+export ZSH_PLUGINS="$ZSH/plugins"
+export ZSH_THEMES="$ZSH/themes"
+
+# Load all of the config files in ~/my-zsh/lib that end in .zsh
+for config_file ($ZSH/lib/*.zsh); do
+  source $config_file
+done
+
+# Load plugins
+source $ZSH_PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Load theme
+source $ZSH_THEMES/simple.zsh
+
+# Load profile
+source $ZSH/profile
---
+++ themes/simple.zsh
@@ -0,0 +1,3 @@
+autoload -U colors && colors
+
+PROMPT="%{$fg_bold[blue]%}%1~ %{$fg_bold[black]%}>%{$reset_color%}"
